cc0920fc2c928d42928d057563968c6243e5ecb4
basav <basavaraj26su@gmail.com>
2024-08-08 21:34:36 +0530

    test_gmail test case file has been committed

---
+++ tests/test_gmail.py
@@ -0,0 +1,13 @@
+
+
+
+class Test_abc:
+
+    def test_m1(self):
+        print("I am test method m1 from class Test_abc")
+
+    def test_m2(self):
+        print("I am test method m2 from class Test_abc")
+
+    def test_m3(self):
+        print("I am test method m3 from class Test_abc")
