f5e4c2772bd4e48a9ae29b4be61b4fe576c9bb3c
basav <basavaraj26su@gmail.com>
2024-08-08 22:05:35 +0530

    m3 method has been removed

--- tests/test_gmail.py
+++ tests/test_gmail.py
@@ -8,6 +8,3 @@ class Test_abc:
 
     def test_m2(self):
         print("I am test method m2 from class Test_abc")
-
-    def test_m3(self):
-        print("I am test method m3 from class Test_abc")
